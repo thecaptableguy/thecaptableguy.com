a0c0b510c88b34a977022ba203288511b0c57f11
Marco Santonastasi <marco.santonastasi@altesis-pro.com>
2025-06-03 09:12:33 +0200

    Untrack files specified in .gitignore

--- build.ts
+++ build.ts
@@ -1,10 +1,16 @@
 import bun_plugin_tailwind from "bun-plugin-tailwind"
 
+import { readdir } from "node:fs/promises";
+import path from 'node:path';
+
+const srcFiles = (await readdir('./src/', { recursive: true }))
+    .map(file => path.resolve('./src', file));
+
 await Bun.build({
     target: "browser",
-    entrypoints: ["./src/**/*.html"],
+    entrypoints: srcFiles,
     outdir: "./dist",
     minify: true,
-    splitting: true,
+    //splitting: true,
     plugins: [bun_plugin_tailwind],
 });
