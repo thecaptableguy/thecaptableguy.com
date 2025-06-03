b68a6508e0b5d047f12d8a6e11a4aaefb2ee1f18
Marco Santonastasi <marco.santonastasi@altesis-pro.com>
2025-06-03 08:04:23 +0200

    bun build script to circunvent use of plugins in build step

--- build.ts
+++ build.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+import bun_plugin_tailwind from "bun-plugin-tailwind"
+
+await Bun.build({
+    target: "browser",
+    entrypoints: ["./src/**/*.html"],
+    outdir: "./dist",
+    minify: true,
+    splitting: true,
+    plugins: [bun_plugin_tailwind],
+});
